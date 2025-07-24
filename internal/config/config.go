7e2f6f8ce920a02620f48a1b12044adc80ebb538
IlyaStarshinov <starshinov.ilya@yandex.ru>
2025-07-24 13:25:02 +0300

    I connected the container with postgres, connected the application to the database

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,48 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func LoadConfig() (*Config, error) {
+	// Проверяем, есть ли файл
+	if _, err := os.Stat(".env"); err != nil {
+		return nil, fmt.Errorf(".env file not found: %w", err)
+	}
+
+	// Загружаем .env
+	err := godotenv.Load("C:/Users/ilyas/GolandProjects/onlineSubscriptions/.env")
+	if err != nil {
+		return nil, fmt.Errorf("error loading .env: %w", err)
+	}
+
+	// Проверяем, что переменные не пустые
+	requiredVars := []string{"DB_HOST", "DB_PORT", "DB_USER", "DB_PASSWORD", "DB_NAME"}
+	for _, varName := range requiredVars {
+		if os.Getenv(varName) == "" {
+			return nil, fmt.Errorf("missing required env variable: %s", varName)
+		}
+	}
+
+	cfg := &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+	}
+
+	fmt.Printf("Config loaded: %+v\n", cfg) // Вывод всех полей
+	return cfg, nil
+}
--- internal/model/subscription.go
+++ internal/model/subscription.go
@@ -1,1 +1,15 @@
 package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Subscription struct {
+	ID          uuid.UUID  `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	ServiceName string     `json:"service_name" gorm:"not null"`
+	Price       int        `json:"price" gorm:"not null"`
+	UserID      uuid.UUID  `json:"user_id" gorm:"type:uuid;not null"`
+	StartDate   time.Time  `json:"start_date" gorm:"not null"`
+	EndDate     *time.Time `json:"end_date,omitempty"`
+}
